8094d2168700543b252ed32f84deca179d32df3b
Conor Kennedy <conor@conorkennedy.com>
2022-03-19 14:50:50 -0400

    fixed header sizing and button

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -8,7 +8,7 @@ const Header = () =>{
     return(
         <div className="sticky top-0 z-50">
             <div className="bg-red-700 shadow-lg flex pr-5 pl-5 ">
-                <div className="flex flex-row  h-12 w-18">
+                <div className="flex flex-row  h-14 w-18">
                     <HeaderLogo />
                     <HeaderLinks />
                     <div className="flex flex-row pl-64 ml-auto h-full">
