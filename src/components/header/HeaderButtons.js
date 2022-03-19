e340d2d2bb9d9494fdd85a68e4f32c45d66438ed
Conor Kennedy <conor@conorkennedy.com>
2022-03-19 09:14:52 -0400

    added hover styling  to header

--- src/components/header/HeaderButtons.js
+++ src/components/header/HeaderButtons.js
@@ -1,10 +1,10 @@
 const HeaderButtons = () => {
     return (
         <div>
-            <button className="text-white capitalize text-xl font-normal max-w-max pl-2.5 pr-2.5 m-0 leading-10">
+            <button className="text-white capitalize text-xl font-normal max-w-max pl-2.5 pr-2.5 m-0 leading-10 hover:text-red-300 hover:underline">
                 Login
             </button>
-            <button className="text-white capitalize text-xl font-normal max-w-max pl-2.5 pr-2.5 m-0 leading-10">
+            <button className="text-white capitalize text-xl font-normal max-w-max pl-2.5 pr-2.5 m-0 leading-10 hover:text-red-300 hover:underline">
                 <div>Register</div>
             </button>
         </div>
--- src/components/header/HeaderIcons.js
+++ src/components/header/HeaderIcons.js
@@ -5,17 +5,17 @@ const HeaderIcons = () =>{
     return (
         <div className="items-center flex-row flex">
             <div className="mr-2 ml-2 items-center flex-row flex">
-                <button className="text-white capitalize text-xl font-normal max-w-max pl-2.5 pr-2.5 m-0 leading-10">
+                <button className="text-white capitalize text-xl font-normal max-w-max pl-2.5 pr-2.5 m-0 leading-10 hover:text-red-300">
                     <IoSearch/>
                 </button>
                 </div>
                 <div className="mr-2 ml-2 items-center flex-row flex">
-                    <a href="#" className="text-white capitalize text-xl font-normal max-w-max pl-2.5 pr-2.5 m-0 leading-10 ">
+                    <a href="#" className="text-white capitalize text-xl font-normal max-w-max pl-2.5 pr-2.5 m-0 leading-10 hover:text-red-300">
                         <IoIosNotifications />
                     </a>
                 </div>
                 <div className="mr-2 ml-2 items-center flex-row flex">
-                    <a href="#" className="text-white capitalize text-xl font-normal max-w-max pl-2.5 pr-2.5 m-0 leading-10 ">  
+                    <a href="#" className="text-white capitalize text-xl font-normal max-w-max pl-2.5 pr-2.5 m-0 leading-10 hover:text-red-300">  
                         <IoCart />
                     </a>
             </div>
