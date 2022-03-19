0ddcc0420943eed6119c15296ced53ab285fc72a
Conor Kennedy <conor@conorkennedy.com>
2022-03-19 18:52:53 -0400

    updated readme

--- src/components/CardGrid.js
+++ src/components/CardGrid.js
@@ -1,6 +1,6 @@
 const CardGrid = (props) =>{
     return(
-        <div className="container my-4 mx-auto px-4 md:px-12">
+        <div className="container my-2 mx-auto px-4 md:px-12">
             <div className="flex flex-row -mx-1 lg:-mx-4">
                 {props.children}
             </div>
