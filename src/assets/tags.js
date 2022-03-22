a96f13b5a7092f7a63194a7526d4bf613c376ba4
Conor Kennedy <conor@conorkennedy.com>
2022-03-21 20:58:39 -0400

    added all tags and images to assets file

--- src/assets/tags.js
+++ src/assets/tags.js
@@ -80,4 +80,640 @@ export default [
         title: 'Busty',
         cover: 'https://t.fakku.net/images/upload/oppai-tag.png'
     },
+    {
+        title: 'Catgirl',
+        cover: 'https://t.fakku.net/images/upload/catgirl-tag.png'
+    },
+    {
+        title: 'CG Set',
+        cover: 'https://t.fakku.net/images/upload/cg-set-tag.png'
+    },
+    {
+        title: 'Cheating',
+        cover: 'https://t.fakku.net/images/upload/cheating2-tag.png'
+    },
+    {
+        title: 'Cheerleader',
+        cover: 'https://t.fakku.net/images/upload/cheerleader-tag.png'
+    },
+    {
+        title: 'Chikan',
+        cover: 'https://t.fakku.net/images/upload/chikan-tag.png'
+    },
+    {
+        title: 'Christmas',
+        cover: 'https://t.fakku.net/images/upload/christmas-tag.png'
+    },
+    {
+        title: 'Chubby',
+        cover: 'https://t.fakku.net/images/upload/chubby-tag.png'
+    },
+    {
+        title: 'Color',
+        cover: 'https://t.fakku.net/images/upload/color.jpg'
+    },
+    {
+        title: 'Comedy',
+        cover: 'https://t.fakku.net/images/upload/comedy-tag.png'
+    },
+    {
+        title: 'Condom',
+        cover: 'https://t.fakku.net/images/upload/condom-tag.png'
+    },
+    {
+        title: 'Cosplay',
+        cover: 'https://t.fakku.net/images/upload/cosplay-tag.png'
+    },
+    {
+        title: 'Creampie',
+        cover: 'https://t.fakku.net/images/upload/nakadashi-tag.png'
+    },
+    {
+        title: 'Crossdressing',
+        cover: 'https://t.fakku.net/images/upload/crossdressing-tag.png'
+    },
+    {
+        title: 'Crotch Tattoo',
+        cover: 'https://t.fakku.net/images/upload/crotch-tattoo-tag.png'
+    },
+    {
+        title: 'Cunnilingus',
+        cover: 'https://t.fakku.net/images/upload/cunnilingus-tag.png'
+    },
+    {
+        title: 'Dark Skin',
+        cover: 'https://t.fakku.net/images/upload/dark-skin-tag.png'
+    },
+    {
+        title: 'Deepthroat',
+        cover: 'https://t.fakku.net/images/upload/deepthroat-tag.png'
+    },
+    {
+        title: 'Deity',
+        cover: 'https://t.fakku.net/images/upload/attribute-image-default.jpg'
+    },
+    {
+        title: 'Delinquent',
+        cover: 'https://t.fakku.net/images/upload/attribute-image-default.jpg'
+    },
+    {
+        title: 'Demo',
+        cover: 'https://t.fakku.net/images/upload/game-generic-tag.png'
+    },
+    {
+        title: 'Demon',
+        cover: 'https://t.fakku.net/images/upload/attribute-image-default.jpg'
+    },
+    {
+        title: 'Double Penetration',
+        cover: 'https://t.fakku.net/images/upload/double-penetration-tag.png'
+    },
+    {
+        title: 'Doujin',
+        cover: 'https://t.fakku.net/images/upload/doujin2-tag.png'
+    },
+    {
+        title: 'Dubbed',
+        cover: 'https://t.fakku.net/images/upload/dubbed-tag.png'
+    },
+    {
+        title: 'Ecchi',
+        cover: 'https://t.fakku.net/images/upload/ecchi-tag.png'
+    },
+    {
+        title: 'Elf',
+        cover: 'https://t.fakku.net/images/upload/elf-tag.png'
+    },
+    {
+        title: 'Exhibitionism',
+        cover: 'https://t.fakku.net/images/upload/exhibitionist-tag.png'
+    },
+    {
+        title: 'Eyebrows',
+        cover: 'https://t.fakku.net/images/upload/eyebrows-tag.png'
+    },
+    {
+        title: 'Eyepatch',
+        cover: 'https://t.fakku.net/images/upload/eyepatch-tag.png'
+    },
+    {
+        title: 'Face Mask',
+        cover: 'https://t.fakku.net/images/upload/face-mask-tag.png'
+    },
+    {
+        title: 'Facesitting',
+        cover: 'https://t.fakku.net/images/upload/facesitting-tag.png'
+    },
+    {
+        title: 'Fangs',
+        cover: 'https://t.fakku.net/images/upload/fangs-tag.png'
+    },
+    {
+        title: 'Fantasy',
+        cover: 'https://t.fakku.net/images/upload/fantasy-tag.png'
+    },
+    {
+        title: 'Femboy',
+        cover: 'https://t.fakku.net/images/upload/femboy-tag-v2.png'
+    },
+    {
+        title: 'Femdom',
+        cover: 'https://t.fakku.net/images/upload/femdom3-tag.png'
+    },
+    {
+        title: 'Filming',
+        cover: 'https://t.fakku.net/images/upload/filming-tag.png'
+    },
+    {
+        title: 'Fingering',
+        cover: 'https://t.fakku.net/images/upload/fingering-tag.png'
+    },
+    {
+        title: 'Fishnets',
+        cover: 'https://t.fakku.net/images/upload/attribute-image-default.jpg'
+    },
+    {
+        title: 'Footjob',
+        cover: 'https://t.fakku.net/images/upload/ashikoki-tag.png'
+    },
+    {
+        title: 'Foreigner',
+        cover: 'https://t.fakku.net/images/upload/foreigner-tag.png'
+    },
+    {
+        title: 'Freckles',
+        cover: 'https://t.fakku.net/images/upload/freckles-tag.png'
+    },
+    {
+        title: 'Free',
+        cover: 'https://t.fakku.net/images/upload/free-tag.png'
+    },
+    {
+        title: 'Futanari',
+        cover: 'https://t.fakku.net/images/upload/futanari-tag.png'
+    },
+    {
+        title: 'Game',
+        cover: 'https://t.fakku.net/images/upload/game-tag-image.jpg'
+    },
+    {
+        title: 'Genderbend',
+        cover: 'https://t.fakku.net/images/upload/genderbend2-tag.png'
+    },
+    {
+        title: 'Glasses',
+        cover: 'https://t.fakku.net/images/upload/glasses2-tag.png'
+    },
+    {
+        title: 'Group',
+        cover: 'https://t.fakku.net/images/upload/group-tag.png'
+    },
+    {
+        title: 'Hairy Armpit',
+        cover: 'https://t.fakku.net/images/upload/hairy-armpit-tag.png'
+    },
+    {
+        title: 'Handjob',
+        cover: 'https://t.fakku.net/images/upload/handjob-tag.png'
+    },
+    {
+        title: 'Harem',
+        cover: 'https://t.fakku.net/images/upload/harem-tag.png'
+    },
+    {
+        title: 'Headphones',
+        cover: 'https://t.fakku.net/images/upload/headphones-tag.png'
+    },
+    {
+        title: 'Heart Pupils',
+        cover: 'https://t.fakku.net/images/upload/heartpupils-tag.png'
+    },
+    {
+        title: 'Hentai',
+        cover: 'https://t.fakku.net/images/upload/hentai-tag.png'
+    },
+    {
+        title: 'Historical',
+        cover: 'https://t.fakku.net/images/upload/historical-tag.png'
+    },
+    {
+        title: 'Horns',
+        cover: 'https://t.fakku.net/images/upload/horns-tag.png'
+    },
+    {
+        title: 'Horror',
+        cover: 'https://t.fakku.net/images/upload/horror2-tag.png'
+    },
+    {
+        title: 'Hot Spring',
+        cover: 'https://t.fakku.net/images/upload/attribute-image-default.jpg'
+    },
+    {
+        title: 'Housewife',
+        cover: 'https://t.fakku.net/images/upload/housewife-tag.png'
+    },
+    {
+        title: 'Huge Boobs',
+        cover: 'https://t.fakku.net/images/upload/bakunyuu-tag.png'
+    },
+    {
+        title: 'Idol',
+        cover: 'https://t.fakku.net/images/upload/idol2-tag.png'
+    },
+    {
+        title: 'Illustration',
+        cover: 'https://t.fakku.net/images/upload/illustration-tag.png'
+    },
+    {
+        title: 'Impregnation',
+        cover: 'https://t.fakku.net/images/upload/impregnation-tag.png'
+    },
+    {
+        title: 'Inseki',
+        cover: 'https://t.fakku.net/images/upload/inseki-tag2.png'
+    },
+    {
+        title: 'Interview',
+        cover: 'https://t.fakku.net/images/upload/interview-tag.png'
+    },
+    {
+        title: 'Inverted Nipples',
+        cover: 'https://t.fakku.net/images/upload/inverted-nipples-tag.png'
+    },
+    {
+        title: 'Isekai',
+        cover: 'https://t.fakku.net/images/upload/isekai-tag.png'
+    },
+    {
+        title: 'Kimono',
+        cover: 'https://t.fakku.net/images/upload/yukata-tag.png'
+    },
+    {
+        title: 'Kogal',
+        cover: 'https://t.fakku.net/images/upload/gyaru-tag.png'
+    },
+    {
+        title: 'Kuudere',
+        cover: 'https://t.fakku.net/images/upload/kuudere-tag.png'
+    },
+    {
+        title: 'Lactation',
+        cover: 'https://t.fakku.net/images/upload/lactation-tag.png'
+    },
+    {
+        title: 'Leg Lock',
+        cover: 'https://t.fakku.net/images/upload/attribute-image-default.jpg'
+    },
+    {
+        title: 'Light Novel',
+        cover: 'https://t.fakku.net/images/upload/light-novel-tag.png'
+    },
+    {
+        title: 'Lingerie',
+        cover: 'https://t.fakku.net/images/upload/lingerie-tag.png'
+    },
+    {
+        title: 'Love Hotel',
+        cover: 'https://t.fakku.net/images/upload/love-hotel-tag.png'
+    },
+    {
+        title: 'Magical Girl',
+        cover: 'https://t.fakku.net/images/upload/magical-girl-tag.png'
+    },
+    {
+        title: 'Maid',
+        cover: 'https://t.fakku.net/images/upload/maid-tag.png'
+    },
+    {
+        title: 'Masturbation',
+        cover: 'https://t.fakku.net/images/upload/masturbation2-tag.png'
+    },
+    {
+        title: 'Mating Press',
+        cover: 'https://t.fakku.net/images/upload/attribute-image-default.jpg'
+    },
+    {
+        title: 'Miko',
+        cover: 'https://t.fakku.net/images/upload/miko-tag.png'
+    },
+    {
+        title: 'Milf',
+        cover: 'https://t.fakku.net/images/upload/milf-tag.png'
+    },
+    {
+        title: 'Monster Girl',
+        cover: 'https://t.fakku.net/images/upload/monster-girl-tag-image.jpg'
+    },
+    {
+        title: 'Mosaics',
+        cover: 'https://t.fakku.net/images/upload/censored2-tag.png'
+    },
+    {
+        title: 'Muscles',
+        cover: 'https://t.fakku.net/images/upload/muscle-tag.png'
+    },
+    {
+        title: 'Music',
+        cover: 'https://t.fakku.net/images/upload/music-tag.png'
+    },
+    {
+        title: 'Netorare',
+        cover: 'https://t.fakku.net/images/upload/netorare2-tag.png'
+    },
+    {
+        title: 'Netorase',
+        cover: 'https://t.fakku.net/images/upload/netorase-tag.png'
+    },
+    {
+        title: 'Netori',
+        cover: 'https://t.fakku.net/images/upload/netori-tag.png'
+    },
+    {
+        title: 'Newhalf',
+        cover: 'https://t.fakku.net/images/upload/trans.jpg'
+    },
+    {
+        title: 'Ninja',
+        cover: 'https://t.fakku.net/images/upload/ninja-tag.png'
+    },
+    {
+        title: 'No Sex',
+        cover: 'https://t.fakku.net/images/upload/nosex-tag.png'
+    },
+    {
+        title: 'Non-H',
+        cover: 'https://t.fakku.net/images/upload/non-h-tag.png'
+    },
+    {
+        title: 'Nun',
+        cover: 'https://t.fakku.net/images/upload/nun-tag.png'
+    },
+    {
+        title: 'Nurse',
+        cover: 'https://t.fakku.net/images/upload/nurse-tag.png'
+    },
+    {
+        title: 'Office Lady',
+        cover: 'https://t.fakku.net/images/upload/office-lady-tag.png'
+    },
+    {
+        title: 'Ojousama',
+        cover: 'https://t.fakku.net/images/upload/ojousama-tag.png'
+    },
+    {
+        title: 'Ongoing',
+        cover: 'https://t.fakku.net/images/upload/ongoing-tag.png'
+    },
+    {
+        title: 'Oni',
+        cover: 'https://t.fakku.net/images/upload/oni-tag.png'
+    },
+    {
+        title: 'Orgasm Denial',
+        cover: 'https://t.fakku.net/images/upload/orgasm-denial-tag.png'
+    },
+    {
+        title: 'Osananajimi',
+        cover: 'https://t.fakku.net/images/upload/attribute-image-default.jpg'
+    },
+    {
+        title: 'Outdoors',
+        cover: 'https://t.fakku.net/images/upload/attribute-image-default.jpg'
+    },
+    {
+        title: 'Paizuri',
+        cover: 'https://t.fakku.net/images/upload/paizuri-tag.png'
+    },
+    {
+        title: 'Pantyhose',
+        cover: 'https://t.fakku.net/images/upload/pantyhose-tag.png'
+    },
+    {
+        title: 'Paperback',
+        cover: 'https://t.fakku.net/images/upload/light-novel-tag.png'
+    },
+    {
+        title: 'Pegging',
+        cover: 'https://t.fakku.net/images/upload/pegging-tag.png'
+    },
+    {
+        title: 'Petite',
+        cover: 'https://t.fakku.net/images/upload/pettanko2-tag.png'
+    },
+    {
+        title: 'Piercing',
+        cover: 'https://t.fakku.net/images/upload/attribute-image-default.jpg'
+    },
+    {
+        title: 'Platformer',
+        cover: 'https://t.fakku.net/images/upload/game-generic-tag.png'
+    },
+    {
+        title: 'Ponytail',
+        cover: 'https://t.fakku.net/images/upload/ponytail-tag.png'
+    },
+    {
+        title: 'Pool',
+        cover: 'https://t.fakku.net/images/upload/attribute-image-default.jpg'
+    },
+    {
+        title: 'Pregnant',
+        cover: 'https://t.fakku.net/images/upload/pregnant-png.png'
+    },
+    {
+        title: 'Pubic Hair',
+        cover: 'https://t.fakku.net/images/upload/hairy-tag.png'
+    },
+    {
+        title: 'Puzzle',
+        cover: 'https://t.fakku.net/images/upload/game-generic-tag.png'
+    },
+    {
+        title: 'Qipao',
+        cover: 'https://t.fakku.net/images/upload/qipao-tag.png'
+    },
+    {
+        title: 'Rimjob',
+        cover: 'https://t.fakku.net/images/upload/rimjob-tag.png'
+    },
+    {
+        title: 'Robot',
+        cover: 'https://t.fakku.net/images/upload/attribute-image-default.jpg'
+    },
+    {
+        title: 'Schoolgirl Outfit',
+        cover: 'https://t.fakku.net/images/upload/schoolgirl-tag.png'
+    },
+    {
+        title: 'Sci-Fi',
+        cover: 'https://t.fakku.net/images/upload/scifi-tag.png'
+    },
+    {
+        title: 'Shimapan',
+        cover: 'https://t.fakku.net/images/upload/shimapan-png.png'
+    },
+    {
+        title: 'Shortstack',
+        cover: 'https://t.fakku.net/images/upload/attribute-image-default.jpg'
+    },
+    {
+        title: 'Sixty-Nine',
+        cover: 'https://t.fakku.net/images/upload/sixty-nine-tag.png'
+    },
+    {
+        title: 'Slice of Life',
+        cover: 'https://t.fakku.net/images/upload/slice-of-life-tag.png'
+    },
+    {
+        title: 'Socks',
+        cover: 'https://t.fakku.net/images/upload/socks-tag.png'
+    },
+    {
+        title: 'Spats',
+        cover: 'https://t.fakku.net/images/upload/spats-tag.png'
+    },
+    {
+        title: 'Spirit',
+        cover: 'https://t.fakku.net/images/upload/attribute-image-default.jpg'
+    },
+    {
+        title: 'Sportswear',
+        cover: 'https://t.fakku.net/images/upload/sportswear-tag.png'
+    },
+    {
+        title: 'Spread',
+        cover: 'https://t.fakku.net/images/upload/spread-tag.png'
+    },
+    {
+        title: 'Squirting',
+        cover: 'https://t.fakku.net/images/upload/squirting-tag.jpg'
+    },
+    {
+        title: 'Stockings',
+        cover: 'https://t.fakku.net/images/upload/stockings-tag.png'
+    },
+    {
+        title: 'Story Arc',
+        cover: 'https://t.fakku.net/images/upload/light-novel-tag.png'
+    },
+    {
+        title: 'Subbed',
+        cover: 'https://t.fakku.net/images/upload/subbed-tag.png'
+    },
+    {
+        title: 'Succubus',
+        cover: 'https://t.fakku.net/images/upload/succubus-tag.png'
+    },
+    {
+        title: 'Swimsuit',
+        cover: 'https://t.fakku.net/images/upload/swimsuit-tag.png'
+    },
+    {
+        title: 'Swinging',
+        cover: 'https://t.fakku.net/images/upload/swinging-tag.png'
+    },
+    {
+        title: 'Tall Girl',
+        cover: 'https://t.fakku.net/images/upload/attribute-image-default.jpg'
+    },
+    {
+        title: 'Tanlines',
+        cover: 'https://t.fakku.net/images/upload/tanlines-tag.png'
+    },
+    {
+        title: 'Tattoo',
+        cover: 'https://t.fakku.net/images/upload/attribute-image-default.jpg'
+    },
+    {
+        title: 'Teacher',
+        cover: 'https://t.fakku.net/images/upload/teacher-tag.png'
+    },
+    {
+        title: 'Thigh Job',
+        cover: 'https://t.fakku.net/images/upload/attribute-image-default.jpg'
+    },
+    {
+        title: 'Tomboy',
+        cover: 'https://t.fakku.net/images/upload/tomboy-tag.png'
+    },
+    {
+        title: 'Toys',
+        cover: 'https://t.fakku.net/images/upload/toys-tag.png'
+    },
+    {
+        title: 'Trans',
+        cover: 'https://t.fakku.net/images/upload/trans-tag.png'
+    },
+    {
+        title: 'Tsundere',
+        cover: 'https://t.fakku.net/images/upload/tsundere-tag.png'
+    },
+    {
+        title: 'Twintails',
+        cover: 'https://t.fakku.net/images/upload/twintails-tag.png'
+    },
+    {
+        title: 'Ugly Bastard',
+        cover: 'https://t.fakku.net/images/upload/ugly-bastard-tag.png'
+    },
+    {
+        title: 'Uncensored',
+        cover: 'https://t.fakku.net/images/upload/uncensored2-tag.png'
+    },
+    {
+        title: 'Unlimited',
+        cover: 'https://t.fakku.net/images/upload/subscription2-tag.png'
+    },
+    {
+        title: 'Vanilla',
+        cover: 'https://t.fakku.net/images/upload/vanilla-tag.png'
+    },
+    {
+        title: 'Voyeurism',
+        cover: 'https://t.fakku.net/images/upload/voyeurism-tag.png'
+    },
+    {
+        title: 'Vtuber',
+        cover: 'https://t.fakku.net/images/upload/attribute-image-default.jpg'
+    },
+    {
+        title: 'Waitress',
+        cover: 'https://t.fakku.net/images/upload/waitress-tag.png'
+    },
+    {
+        title: 'Webtoon',
+        cover: 'https://t.fakku.net/images/upload/light-novel-tag.png'
+    },
+    {
+        title: 'Western',
+        cover: 'https://t.fakku.net/images/upload/western.jpg'
+    },
+    {
+        title: 'Whiteout',
+        cover: 'https://t.fakku.net/images/upload/whiteout-tag.png'
+    },
+    {
+        title: 'Witch',
+        cover: 'https://t.fakku.net/images/upload/attribute-image-default.jpg'
+    },
+    {
+        title: 'Wrestling',
+        cover: 'https://t.fakku.net/images/upload/wrestling-tag.png'
+    },
+    {
+        title: 'X-ray',
+        cover: 'https://t.fakku.net/images/upload/xray-tag.png'
+    },
+    {
+        title: 'Yandere',
+        cover: 'https://t.fakku.net/images/upload/yandere-tag.png'
+    },
+    {
+        title: 'Yaoi',
+        cover: 'https://t.fakku.net/images/upload/yaoi-tag.png'
+    },
+    {
+        title: 'Yuri',
+        cover: 'https://t.fakku.net/images/upload/yuri-tag.png'
+    },
 ]
