0987533106590127e3bdc2c360ad85b3a279de32
Conor Kennedy <conor@conorkennedy.com>
2022-04-03 20:13:34 -0400

    updated to react 18 | improved responsive ness on header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -8,10 +8,10 @@ const Header = () =>{
     return(
         <div className="sticky top-0 z-50">
             <div className="bg-red-700 shadow-lg flex pr-5 pl-5 ">
-                <div className="ml-4 flex  h-14 w-18 pl-4">
+                <div className="ml-4 flex h-14 pl-4">
                     <HeaderLogo />
                     <HeaderLinks />
-                    <div className="flex pl-80">
+                    <div className="flex  lg:pl-40 3xl:pl-80">
                         <HeaderIcons />
                         <HeaderButtons /> 
                     </div>
